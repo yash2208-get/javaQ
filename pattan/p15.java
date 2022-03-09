05241b0b4c2ee5461c318cc368aed8f25e0f509d
yash2208-get <yash123.ajmer@gmail.com>
2022-03-10 00:13:57 +0530

    Codes

--- pattan/p15.java
+++ pattan/p15.java
@@ -1,20 +1,22 @@
 import java.util.*;
-class pttan10 {
+class pttan15 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int row = 1 , nspaces = n/2 , nstars = 1;
+        int row = 1 , nspaces = n/2 , nstars = 1,num=1;
         while(row <= n){
             for(int i = 1 ; i <= nspaces ; i++){
                 System.out.print("\t");
-            }             
-            for(int i = 1 ; i <= nstars ; i++){
-                if(i==1 || i==nstars){
-                    System.out.print("*\t");
+            }    int temp=num;         
+            for(int i = 1 ; i <= nstars ; i++)
+            {
+                 
+                System.out.print(temp +"\t");
+                if(i <= nstars/2){
+                    temp=temp+1;
                 }
-                else
-                {
-                    System.out.print("\t");
+                else{
+                    temp=temp-1;
                 }
                 
             }
@@ -24,9 +26,11 @@ class pttan10 {
             if(row <= n/2){ 
                 nspaces = nspaces - 1;
                 nstars = nstars + 2;
+                num++;
             }else{ 
                 nspaces = nspaces + 1;
                 nstars = nstars - 2;
+                num=num-1;
             }
             row++;
         }
